89f5d11f694cabd0d24b65bbaa3baf5ca81b0904
Felipe Ballesteros <felipeballesteros@msn.com>
2016-05-21 23:16:59 -0400

    adding nester routes for testing - routes.js change

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,15 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 
+const Greeting = () => {
+	return <div>Hey there!</div>
+}
+
 export default(
 	// Root of the app will render the App component
-	<Route path="/" component={App} />
+	<Route path="/" component={App}>
+		<Route path="greet" component={Greeting} />
+		<Route path="greet2" component={Greeting} />
+ 		<Route path="greet3" component={Greeting} />
+	</Route>
 );
