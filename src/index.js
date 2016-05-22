54a47cb9f170372969cd9c752d54e7f768866211
Felipe Ballesteros <felipeballesteros@msn.com>
2016-05-21 22:16:08 -0400

    setup initial configuration for react router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, browserhistory } from 'react-router';
 
-import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<Router history={browserhistory} />
+	</Provider>
+	, document.querySelector('.container'));
